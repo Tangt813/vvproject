53c667aed072516a432498bbe6734e35a3ce6567
tangt813201 <78403006+Tangt813@users.noreply.github.com>
2022-02-27 20:18:43 +0800

    博哥1

--- src/views/workbench/dashboard.vue
+++ src/views/workbench/dashboard.vue
@@ -1,165 +1,82 @@
 <template>
-  <el-row class="wrap">
-    <el-col :span="24" class="wrap-breadcrumb">
-      <el-breadcrumb seporate="/">
-        <el-breadcrumb-item :to="{ path: '/'}"><b>首页</b></el-breadcrumb-item>
-      </el-breadcrumb>
-      <el-col :span="24" class="wrap-main">
-        <section class="chart-container">
-          <el-row>
-            <el-col :span="8">
-              <el-card :body-style="{ padding: '0px'}">
-                <img src="../../assets/images/card.jpg">
-                <div style="padding: 12px;">
-                  <span>标题</span>
-                  <div class="bottom clearfix">
-                    <time class="time">2018-14-19</time>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="8">
-              <el-card :body-style="{ padding: '0px'}">
-                <img src="../../assets/images/card.jpg">
-                <div style="padding: 12px;">
-                  <span>标题</span>
-                  <div class="bottom clearfix">
-                    <time class="time">2018-14-19</time>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="8">
-              <el-card :body-style="{ padding: '0px'}">
-                <img src="../../assets/images/card.jpg">
-                <div style="padding: 12px;">
-                  <span>标题</span>
-                  <div class="bottom clearfix">
-                    <time class="time">2018-14-19</time>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="12">
-              <div id="chartColumn" style="width: 100%; height: 400px;"></div>
-            </el-col>
-            <el-col :span="12">
-              <div id="chartBar" style="width: 100%; height: 400px;"></div>
-            </el-col>
-            <el-col :span="12"></el-col>
-            <el-col :span="12"></el-col>
-            <el-col :span="24"></el-col>
-          </el-row>
-        </section>
-      </el-col>
-    </el-col>
-  </el-row>
+  <div style="height:100%;width:100%;position:fixed;background:#81DAF5">
+    <div style="width:160px;border:0.2px solid #000;background:#81DAF5;text-align:center;margin-left:5px;margin-top:5px">
+      <font style="color:white;text-align:center;width:100%">车辆实时信息</font>
+      <div v-for="(car_data,i) in real_time_car_data" style="text-align:left;font-size:12px">
+        <div style="display:flex;margin-top:2px">
+          <div style="margin-top:2px" >
+            <font style="color:white">{{i+1}}.</font>
+          </div>
+          <div >
+            <font style="color:white">{{car_data.name}}</font>
+            <font style="color:white">{{car_data.state}}</font>
+            <br>
+            <font style="color:white">容量{{car_data.capacity[0]}},{{car_data.capacity[1]}}</font>
+            <br>
+            <font style="color:white">位置({{car_data.location[0]}},{{car_data.location[1]}})</font>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+    <br>
+
+    <div style="width:160px;border:0.2px solid #000;background:#81DAF5;text-align:center;margin-left:5px;margin-top:5px">
+      <font style="color:white;text-align:center;width:100%">充电站实时信息</font>
+      <div v-for="(station_data,i) in real_time_charge_station" style="text-align:left;font-size:12px">
+        <div style="display:flex;margin-top:2px">
+          <div style="margin-top:2px" >
+            <font style="color:white">{{i+1}}.</font>
+          </div>
+          <div >
+            <font style="color:white">{{station_data.name}}</font>
+            <font style="color:white">桩数{{station_data.pole_number}}</font>
+            <br>
+            <font style="color:white">使用率{{station_data.usage_rate}}</font>
+            <br>
+            <font style="color:white">功率{{station_data.power}}</font>
+            <br>
+            <font style="color:white">位置({{station_data.location[0]}},{{station_data.location[1]}})</font>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
 </template>
+
 <style>
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .image {
-    width: 100%;
-    display: block;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-
-  .clearfix:after {
-    clear: both
-  }
-
-  .chart-container {
-    width: 100%;
-  }
-  .chart-container .el-col {
-    padding: 30px 20px;
-  }
 </style>
 <script>
-  import echarts from 'echarts'
-  export default {
-    name: 'dashboard',
-    data(){
-      return {
-        chartColumn: null,
-        chartBar: null
-      }
-    },
-    mounted (){
-      this.drawShape();
-    },
-    methods: {
-      drawShape(){
-        this.chartColumn = echarts.init(document.getElementById('chartColumn'));
-        this.chartBar = echarts.init(document.getElementById('chartBar'));
+export default{
+  data(){
+    return{
+      real_time_car_data:[
+        {
+          name:"A1车",
+          state:"行驶",
+          capacity:["40kWh","SOC98%"],
+          location:["119°21'","30°03'"],
 
-        this.chartColumn.setOption({
-          title: { text: 'Column Chart' },
-          tooltip: {},
-          xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line'
-          }]
-        });
 
-        this.chartBar.setOption({
-          color: ['#3398DB'],
-          tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-              type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis : [
-            {
-              type : 'category',
-              data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-              axisTick: {
-                alignWithLabel: true
-              }
-            }
-          ],
-          yAxis : [
-            {
-              type : 'value'
-            }
-          ],
-          series : [
-            {
-              name:'直接访问',
-              type:'bar',
-              barWidth: '60%',
-              data:[10, 52, 200, 334, 390, 330, 220]
-            }
-          ]
-        });
-      }
+
+        }
+      ],
+      real_time_charge_station:[
+        {
+          name:"a1站",
+          pole_number:"12",
+          usage_rate:"50%",
+          power:"360kw",
+          location:["119°61'","30°03'"]
+        }
+      ]
+
     }
   }
+}
 </script>
